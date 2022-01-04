1b35372b5bccb11d1b8aa1154a9bc1a6704cd393
Arindam2002 <79254056+Arindam2002@users.noreply.github.com>
2022-01-04 11:11:23 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="left-panel">
+      <div className="d-flex flex-column">
+        <h1>skajdna</h1>
+        <p>dmfd</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
